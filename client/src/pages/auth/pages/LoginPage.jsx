d9ea353f8b2fbebf90b2d3397ea5ea5ccd535ca7
javahir1223 <javah1223@gmail.com>
2024-11-23 15:08:24 +0500

    not full auth(resp left)

--- client/src/pages/auth/pages/LoginPage.jsx
+++ client/src/pages/auth/pages/LoginPage.jsx
@@ -7,7 +7,7 @@ const LoginPage = () => (
 
     <Login />
     </div>
-    <div className="bg-container"><h1>Welcome Back!</h1></div>
+    <div className="bg-container"><h1><img src="https://static10.tgstat.ru/channels/_0/ce/cea270b9585ed6f947f77b3d4cd0c5ae.jpg" alt="" /> <br />Welcome Back!</h1></div>
   </div>
 );
 
